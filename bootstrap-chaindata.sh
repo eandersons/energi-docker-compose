9f3a4c8e5723977af922d8b5ce582cbecf1a7230
Edgars <Edgars+GitHub@gaitenis.id.lv>
2021-04-07 08:23:45 +0300

    Update comment in `bootstrap-chaindata.sh`

--- bootstrap-chaindata.sh
+++ bootstrap-chaindata.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# A wrapper script to apply preimages for Energi Core Node container
+# A wrapper script to load chain data in the Energi Core Node container
 docker-compose stop
 docker-compose run --entrypoint '/bin/sh bootstrap-chaindata.sh' --rm core
 docker-compose up --detach
