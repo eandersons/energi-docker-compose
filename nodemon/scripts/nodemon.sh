993451a19c9325f8cff1be085e2ba81b99da0282
Vulpecula <Vulpecula-nl@users.noreply.github.com>
2021-05-27 08:52:43 +0200

    Fix directory logs
    Fix for error message that log/nodemon.log does not exist.

--- nodemon/scripts/nodemon.sh
+++ nodemon/scripts/nodemon.sh
@@ -92,7 +92,7 @@ function CTRL_C () {
  MNTOTALNRG=0
  USRNAME=$( find /home -name nodekey  2>&1 | grep -v "Permission denied" | awk -F\/ '{print $3}' )
  export PATH=$PATH:/home/${USRNAME}/energi3/bin
- LOGDIR="/home/${USRNAME}/log"
+ LOGDIR="/home/${USRNAME}/logs"
  LOGFILE="${LOGDIR}/nodemon.log"
 
  #+ Modified for `energi3-docker-compose`.
