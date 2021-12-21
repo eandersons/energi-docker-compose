dcc7dde707b3682705394b3bde86f4800a89e63f
Edgars <Edgars+GitHub@gaitenis.id.lv>
2021-12-21 10:46:30 +0200

    Replace `echo` with `printf` in original scripts
    Additionally code was fixed according to `shellcheck` suggestions.

--- nodemon/scripts/nodemon_helper.sh
+++ nodemon/scripts/nodemon_helper.sh
@@ -3,20 +3,20 @@
 function override_read {
   if [[ "${INTERACTIVE}" == 'n' ]]
   then
-    echo $1
-    REPLY=$1
+    printf '%s' "${1}"
+    REPLY="${1}"
   else
-    read -e -i "$1" -r
+    read -e -i "${1}" -r
   fi
 }
 
 function ip_address {
   if [[ "${ECNM_SHOW_IP_EXTERNAL:-n}" == 'y' ]]
   then
-    IP_ADDRESS=$( wget -qO- https://api.ipify.org )
+    IP_ADDRESS="$( wget -qO- https://api.ipify.org )"
   else
-    IP_ADDRESS=$( hostname -i )
+    IP_ADDRESS="$( hostname -i )"
   fi
 
-  echo ${IP_ADDRESS}
+  printf '%s' "${IP_ADDRESS}"
 }
