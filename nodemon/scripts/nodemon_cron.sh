20748d1531882404252bd0d7464f748dd8858df8
Edgars <Edgars+GitHub@gaitenis.id.lv>
2021-12-21 10:46:30 +0200

    Replace `echo` with `printf` in original scripts
    Additionally code was fixed according to `shellcheck` suggestions.

--- nodemon/scripts/nodemon_cron.sh
+++ nodemon/scripts/nodemon_cron.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 while :
 do
-    /bin/bash nodemon.sh cron && {
-        sleep ${ECNM_INTERVAL:-'10m'}
-    } || {
-        echo "Exiting Energi Core Node Monitor container cron because of an error."
-        echo "Please see the output above for details!"
-        break
-    }
+  if /bin/bash nodemon.sh cron
+  then
+    sleep "${ECNM_INTERVAL:-'10m'}"
+  else
+    printf '%s\n%s\n\n' \
+      "Exiting Energi Core Node Monitor container cron because of an error." \
+      "Please see the output above for details!"
+    break
+  fi
 done
