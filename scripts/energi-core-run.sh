981b595b76c21ad2b44a65479996ff06c758e077
Edgars <Edgars+GitHub@gaitenis.id.lv>
2022-03-09 07:06:30 +0200

    Upgrade to Energi Core Node 3.1.2

--- scripts/energi-core-run.sh
+++ scripts/energi-core-run.sh
@@ -2,7 +2,7 @@
 # A script to be used as a Docker container entrypoint to run
 # Energi Core node and unlock account for staking.
 
-/usr/sbin/sshd -f "${SSHD_DIR}/sshd_config"
+/usr/sbin/sshd.pam -f "${SSHD_DIR}/sshd_config"
 
 # https://wiki.energi.world/en/3-1/advanced/core-node-linux#h-2212-mainnet (
 # section "2.2.1.2 Mainnet")
