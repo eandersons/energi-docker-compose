515372628359944d82d590bcbec171b31e603057
Edgars <Edgars+GitHub@gaitenis.id.lv>
2024-07-16 09:13:50 +0300

    Update chain data bootstrapping script
    The chain data bootstrapping script `scripts/bootstrap-chaindata.sh` was
    updated according to the current Wiki content.

--- scripts/bootstrap-chaindata.sh
+++ scripts/bootstrap-chaindata.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 # A script to bootstrap chaindata for Energi Core Node container
-# https://docs.energi.software/en/core-node-troubleshoot#bootstrap
 . ./energi_command.sh
 cd "${ENERGI_CORE_DIR}" || exit
 yes | ${energi_command} removedb
-curl -s https://s3-us-west-2.amazonaws.com/download.energi.software/releases/chaindata/mainnet/gen3-chaindata.tar.gz \
-  | tar xvz --strip-components 1
+# https://wiki.energi.world/docs/guides/bootstrap-core-node#2-running-the-bash-script
+bash -ic "$(
+  wget -4qO- -o- \
+    raw.githubusercontent.com/energicryptocurrency/energi3-provisioning/master/scripts/linux/sync-core-node.sh
+)"
+# shellcheck source=/dev/null
+. ~/.bashrc
